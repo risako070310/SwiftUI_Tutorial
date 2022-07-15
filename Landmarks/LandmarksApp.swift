a2d0c8f333ef5498cfebe2b16b5395eb82eb6ff7
risako070310 <risako070310@gmail.com>
2022-07-15 14:15:56 +0900

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Risako Kato on 2022/07/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
